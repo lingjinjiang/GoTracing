2e6fa2a765acc2d41f21c35bc4f3a0ba909013d1
Ling Jinjiang <lingjinjiang@gmail.com>
2020-03-18 19:39:23 +0800

    use go-ini to load configuration file

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import "github.com/go-ini/ini"
+
+type Configuration struct {
+	Output string
+	Width  float64
+	Height float64
+	Sample int
+}
+
+func NewConfiguration(filename string) (*Configuration, error) {
+	context, err := ini.Load(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	config := Configuration{}
+
+	imageInfo := context.Section("image")
+	if imageInfo != nil {
+		config.Width, err = imageInfo.Key("width").Float64()
+		if err != nil {
+			return nil, err
+		}
+
+		config.Height, err = imageInfo.Key("height").Float64()
+		if err != nil {
+			return nil, err
+		}
+
+		config.Sample, err = imageInfo.Key("sample").Int()
+		if err != nil {
+			return nil, err
+		}
+
+		config.Output = imageInfo.Key("path").String()
+	}
+
+	return &config, nil
+}
